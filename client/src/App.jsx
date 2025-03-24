b395e586ccade59bd9c00dcad0524ccc7a5c949f
Emil George <emil.george7@gmail.com>
2025-03-24 11:03:27 +0000

    feat: create bare bone react client

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import Layout from "./components/Layout";
+
+function App() {
+  return (
+    <div className="flex flex-col h-screen">
+      <Layout />
+    </div>
+  );
+}
+
+export default App;
